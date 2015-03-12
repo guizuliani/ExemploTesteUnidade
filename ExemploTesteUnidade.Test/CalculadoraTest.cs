ed7a7f471f6b5ad189fe4e55c645e015bad79864
Guilherme Zuliani <gui.zuliani@gmail.com>
2015-03-11 22:39:24 -0300

    Implementado a classe calculadora e testes de unidade para o método somar.

---
+++ ExemploTesteUnidade.Test/CalculadoraTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExemploTesteUnidade.Test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+
+        [TestMethod]
+        public void DeveSomarDezComCincoEResultadoDeveSerQuinze()
+        {
+            var target = new Calculadora();
+
+            decimal expected = 15;
+            decimal actual = 0;
+
+            actual = target.Somar(10, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DeveSomarVinteComQuinzeEResultadoDeveSerTrintaECinco()
+        {
+            var target = new Calculadora();
+
+            decimal expected = 35;
+            decimal actual = 0;
+
+            actual = target.Somar(20, 15);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ ExemploTesteUnidade/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExemploTesteUnidade
+{
+    public class Calculadora
+    {
+
+        public decimal Somar(decimal primeiroValor, decimal segundoValor)
+        {
+            return primeiroValor + segundoValor;
+        }
+
+        public decimal Subtrair(decimal primeiroValor, decimal segundoValor)
+        {
+            return primeiroValor - segundoValor;
+        }
+
+        public decimal Multiplicar(decimal primeiroValor, decimal segundoValor)
+        {
+            return primeiroValor * segundoValor;
+        }
+
+        public decimal Dividir(decimal primeiroValor, decimal segundoValor)
+        {
+            if (segundoValor == 0)
+                throw new ArgumentException("Valor do parâmetro inválido.", "segundoValor");
+
+            return primeiroValor / segundoValor;
+        }
+
+    }
+}
